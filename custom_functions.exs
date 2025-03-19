3133ace2bc73ca8f1bd297496664d12bc24ab2d1
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-03-19 23:29:37 +0100

    added non indexing variations

--- custom_functions.exs
+++ custom_functions.exs
@@ -1,42 +1,44 @@
-defmodule CustomFunctions do
-  def loop(array, fun, index \\ 0) do
-    if index < length(array) do
-      fun.(Enum.at(array, index))
-      loop(array, fun, index + 1)
+# without indexing
+defmodule CustomFunctions2 do
+  def loop(list, fun) do
+    case list do
+      [] ->
+        nil
+
+      _ ->
+        fun.(hd(list))
+        loop(tl(list), fun)
     end
   end
 
-  def filter(array, condition, index \\ 0) do
-    if index >= length(array) do
-      []
-    else
-      remaining = filter(array, condition, index + 1)
+  def filter(list, condition) do
+    case list do
+      [] ->
+        []
 
-      if condition.(Enum.at(array, index)) do
-        [Enum.at(array, index)] ++ remaining
-      else
-        remaining
-      end
+      _ ->
+        remaining = filter(tl(list), condition)
+
+        if condition.(hd(list)) do
+          [hd(list)] ++ remaining
+        else
+          remaining
+        end
     end
   end
 
-  def map(array, transform, index \\ 0) do
-    if index >= length(array) do
-      []
-    else
-      remaining = map(array, transform, index + 1)
-
-      [transform.(Enum.at(array, index))] ++ remaining
+  def map(list, transform) do
+    case list do
+      [] -> []
+      _ -> [transform.(hd(list))] ++ map(tl(list), transform)
     end
   end
 
-  def take(array, up_to, index \\ 0) do
-    if index >= length(array) || index >= up_to do
-      []
-    else
-      remaining = take(array, up_to, index + 1)
-
-      [Enum.at(array, index)] ++ remaining
+  def take(list, up_to) do
+    cond do
+      list == [] -> []
+      up_to <= 0 -> []
+      true -> [hd(list)] ++ take(tl(list), up_to - 1)
     end
   end
 
@@ -47,36 +49,101 @@ defmodule CustomFunctions do
     end
   end
 
-  def reverse(array, index \\ 0) do
-    if index >= length(array) do
-      []
-    else
-      reverse(array, index + 1) ++ [Enum.at(array, index)]
+  def reverse(list) do
+    case list do
+      [] -> []
+      _ -> reverse(tl(list)) ++ [hd(list)]
     end
   end
 
-  # def filter_reduce(array, condition, index \\ 0) do
+  # def filter_reduce(list, condition, index \\ 0) do
   # end
 
-  def map_reduce(array, transform) do
-    array
+  def map_reduce(list, transform) do
+    list
     |> reduce([], fn x, acc -> [transform.(x) | acc] end)
     |> reverse()
   end
 end
 
-array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# with indexing
+defmodule CustomFunctions1 do
+  def loop(list, fun, index \\ 0) do
+    if index < length(list) do
+      fun.(Enum.at(list, index))
+      loop(list, fun, index + 1)
+    end
+  end
 
-# CustomFunctions.loop(array, &IO.puts/1)
-CustomFunctions.filter(array, &(rem(&1, 2) != 0))
-|> CustomFunctions.map(&(&1 * 2))
-# |> CustomFunctions.take(5)
+  def filter(list, condition, index \\ 0) do
+    if index >= length(list) do
+      []
+    else
+      remaining = filter(list, condition, index + 1)
+
+      if condition.(Enum.at(list, index)) do
+        [Enum.at(list, index)] ++ remaining
+      else
+        remaining
+      end
+    end
+  end
+
+  def map(list, transform, index \\ 0) do
+    if index >= length(list) do
+      []
+    else
+      remaining = map(list, transform, index + 1)
+
+      [transform.(Enum.at(list, index))] ++ remaining
+    end
+  end
+
+  def take(list, up_to, index \\ 0) do
+    if index >= length(list) || index >= up_to do
+      []
+    else
+      remaining = take(list, up_to, index + 1)
+
+      [Enum.at(list, index)] ++ remaining
+    end
+  end
+
+  def reduce(list, accumulator, fun) do
+    case list do
+      [] -> accumulator
+      _ -> reduce(tl(list), fun.(hd(list), accumulator), fun)
+    end
+  end
+
+  def reverse(list, index \\ 0) do
+    if index >= length(list) do
+      []
+    else
+      reverse(list, index + 1) ++ [Enum.at(list, index)]
+    end
+  end
+
+  # def filter_reduce(list, condition, index \\ 0) do
+  # end
+
+  def map_reduce(list, transform) do
+    list
+    |> reduce([], fn x, acc -> [transform.(x) | acc] end)
+    |> reverse()
+  end
+end
+
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+|> CustomFunctions1.filter(&(rem(&1, 2) != 0))
+|> CustomFunctions1.map(&(&1 * 2))
+|> CustomFunctions1.take(5)
 |> IO.inspect()
+|> CustomFunctions1.loop(&IO.puts/1)
 
-CustomFunctions.filter(array, &(rem(&1, 2) != 0))
-|> CustomFunctions.map_reduce(&(&1 * 2))
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+|> CustomFunctions2.filter(&(rem(&1, 2) != 0))
+|> CustomFunctions2.map(&(&1 * 2))
+|> CustomFunctions2.take(5)
 |> IO.inspect()
-
-# fun = &(&1 * 2)
-# CustomFunctions.reduce(array, 0, &(&1 + &2))
-# |> IO.inspect()
+|> CustomFunctions2.loop(&IO.puts/1)
